574b6bf827ac090a260fe23ccd5aa4821529663f
eduardo <171377664+barbosa-eduardo@users.noreply.github.com>
2024-06-20 11:26:06 -0300

    Create grid with variable size

--- scrips.js
+++ scrips.js
@@ -1,17 +1,21 @@
 const grid = document.querySelector(".grid");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
+createGrid(64);
 
-        square.addEventListener("mouseover", () => {
-            square.classList.add("selected");
-        });
+function createGrid (size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
 
-        row.appendChild(square);
+            square.addEventListener("mouseover", () => {
+                square.classList.add("selected");
+            });
+
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
 }
